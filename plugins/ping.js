1eb33b1ee47a565e885cbe0a1b766d17b7ce0225
sewmika <chamidusewmika@gmail.com>
2025-08-13 10:47:19 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -41,7 +41,7 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363348739987203@newsletter',
+                    newsletterJid: '120363399578502409@newsletter',
                     newsletterName: "𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃",
                     serverMessageId: 143
                 }
